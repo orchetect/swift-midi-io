de256be94852b072f719c53b3dca10f390d378b7
Steffan Andrews <stef@orchetect.com>
2026-04-20 16:16:12 -0700

    Migrated tests from MIDIKit

---
+++ Tests/SwiftMIDIIOTests/MIDIEndpoint/MIDIEndpointIdentity Tests.swift
@@ -0,0 +1,81 @@
+//
+//  MIDIEndpointIdentity Tests.swift
+//  swift-midi • https://github.com/orchetect/swift-midi
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+#if !os(tvOS) && !os(watchOS)
+
+@testable import SwiftMIDIIO
+import Testing
+
+@Suite struct MIDIEndpointIdentity_Tests {
+    @Test
+    func endpoint_OutputEndpoint() {
+        var endpoint = MIDIOutputEndpoint(from: 10_000_000)
+        endpoint.uniqueID = 10_000_001
+        
+        let criteria = MIDIEndpointIdentity.endpoint(endpoint)
+        
+        switch criteria {
+        case let .uniqueID(uID):
+            #expect(uID == 10_000_001)
+            
+        case .uniqueIDWithFallback(
+            id: let uID,
+            fallbackDisplayName: _
+        ):
+            #expect(uID == 10_000_001)
+            
+        default:
+            Issue.record()
+        }
+    }
+    
+    @Test
+    func endpoint_InputEndpoint() {
+        var endpoint = MIDIInputEndpoint(from: 10_000_000)
+        endpoint.uniqueID = 10_000_001
+        
+        let criteria = MIDIEndpointIdentity.endpoint(endpoint)
+        
+        switch criteria {
+        case let .uniqueID(uID):
+            #expect(uID == 10_000_001)
+            
+        case .uniqueIDWithFallback(
+            id: let uID,
+            fallbackDisplayName: _
+        ):
+            #expect(uID == 10_000_001)
+            
+        default:
+            Issue.record()
+        }
+    }
+    
+    @Test
+    func endpoint_asAnyEndpoint() {
+        var endpoint = MIDIInputEndpoint(from: 10_000_000)
+        endpoint.uniqueID = 10_000_001
+        let anyEndpoint = endpoint.asAnyEndpoint()
+        
+        let criteria = MIDIEndpointIdentity.endpoint(anyEndpoint)
+        
+        switch criteria {
+        case let .uniqueID(uID):
+            #expect(uID == 10_000_001)
+            
+        case .uniqueIDWithFallback(
+            id: let uID,
+            fallbackDisplayName: _
+        ):
+            #expect(uID == 10_000_001)
+            
+        default:
+            Issue.record()
+        }
+    }
+}
+
+#endif
---
+++ Tests/SwiftMIDIIOTests/MIDIManager/MIDIManager Tests.swift
@@ -0,0 +1,73 @@
+//
+//  MIDIManager Tests.swift
+//  swift-midi • https://github.com/orchetect/swift-midi
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+#if !os(tvOS) && !os(watchOS)
+
+import CoreMIDI
+@testable import SwiftMIDIIO
+import Testing
+
+@Suite struct MIDIManager_Tests {
+    static let clientName = UUID().uuidString
+    
+    let manager = MIDIManager(
+        clientName: clientName,
+        model: "SwiftMIDI123",
+        manufacturer: "SwiftMIDI"
+    )
+    
+    @Test
+    func midiIO_Manager_defaults() throws {
+        // just check defaults without calling .start() on the manager
+        
+        #expect(manager.clientName == Self.clientName)
+        #expect(manager.model == "SwiftMIDI123")
+        #expect(manager.manufacturer == "SwiftMIDI")
+        #expect(manager.coreMIDIClientRef == MIDIClientRef())
+        
+        #expect(manager.managedInputConnections.isEmpty)
+        #expect(manager.managedOutputConnections.isEmpty)
+        #expect(manager.managedInputs.isEmpty)
+        #expect(manager.managedOutputs.isEmpty)
+        #expect(manager.managedThruConnections.isEmpty)
+        #expect(
+            try manager.unmanagedPersistentThruConnections(
+                ownerID: Bundle.main
+                    .bundleIdentifier ?? "nil"
+            )
+            .isEmpty
+        )
+    }
+    
+    @Test
+    func midiManaged() {
+        // we just want to test the API
+        
+        // public protocol
+        
+        class Foo: MIDIManaged {
+            var api: CoreMIDIAPIVersion = .legacyCoreMIDI
+        }
+        
+        let foo = Foo()
+        
+        _ = foo.api
+        
+        // internal protocol
+        
+        class Bar: MIDIManaged {
+            var midiManager: MIDIManager?
+            var api: CoreMIDIAPIVersion = .legacyCoreMIDI
+        }
+        
+        let bar = Bar()
+        
+        _ = bar.api
+        _ = bar.midiManager
+    }
+}
+
+#endif
