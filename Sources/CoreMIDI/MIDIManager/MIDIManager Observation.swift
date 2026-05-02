7380fd99246e410883f99f62c85265d00cf26d84
Steffan Andrews <stef@orchetect.com>
2026-05-01 20:48:34 -0700

    Updated docs

--- Sources/CoreMIDI/MIDIManager/MIDIManager Observation.swift
+++ Sources/CoreMIDI/MIDIManager/MIDIManager Observation.swift
@@ -12,6 +12,31 @@ import Foundation
 
 extension MIDIManager {
     /// An asynchronous sequence that emits MIDI device information in the system in real-time.
+    ///
+    /// When starting to iterate on the sequence, the initial devices are returned immediately as the first result.
+    /// Thereafter, results are only returned when devices change in the system.
+    ///
+    /// ## Example
+    ///
+    /// In this example, while the view is loaded, the sequence will receive new results any time the system changes.
+    /// The for-loop will automatically exit and `Task` will finish when the view disappears.
+    ///
+    /// ```swift
+    /// struct ContentView: View {
+    ///     @State private var devices: [MIDIDevice] = []
+    ///
+    ///     var body: some View {
+    ///         List(devices) { device in
+    ///             Text(device.name)
+    ///         }
+    ///         .task {
+    ///             for await devices in midiManager.devicesStream() {
+    ///                 self.devices = devices.devices
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
     public func devicesStream() -> AsyncStream<MIDIDevices> {
         AsyncStream { continuation in
@@ -79,6 +104,36 @@ extension MIDIManager.DevicesMonitor: Hashable {
 
 extension MIDIManager {
     /// An asynchronous sequence that emits MIDI endpoint list in the system in real-time.
+    ///
+    /// When starting to iterate on the sequence, the initial endpoints are returned immediately as the first result.
+    /// Thereafter, results are only returned when endpoints change in the system.
+    ///
+    /// ## Example
+    ///
+    /// In this example, while the view is loaded, the sequence will receive new results any time the system changes.
+    /// The for-loop will automatically exit and `Task` will finish when the view disappears.
+    ///
+    /// ```swift
+    /// struct ContentView: View {
+    ///     @State private var inputEndpoints: [MIDIInputEndpoint] = []
+    ///     @State private var outputEndpoints: [MIDIOutputEndpoint] = []
+    ///
+    ///     var body: some View {
+    ///         List(inputEndpoints) { endpoint in
+    ///             Text(endpoint.displayName)
+    ///         }
+    ///         List(outputEndpoints) { endpoint in
+    ///             Text(endpoint.displayName)
+    ///         }
+    ///         .task {
+    ///             for await endpoints in midiManager.endpointsStream() {
+    ///                 inputEndpoints = endpoints.inputs
+    ///                 outputEndpoints = endpoints.outputs
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ```
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
     public func endpointsStream() -> AsyncStream<MIDIEndpoints> {
         AsyncStream { continuation in
